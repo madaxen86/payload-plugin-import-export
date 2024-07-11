69113669420b3808fd422eb2e2732211769d30df
teezeit <tobias.hoelzer@gmail.com>
2024-07-11 18:55:11 +0200

    turn str[]-> str in path argument of import view component

--- src/view/importView.ts
+++ src/view/importView.ts
@@ -9,23 +9,21 @@ export const importViews: (config: Config, pluginOptions?: PluginTypes) => admin
   pluginOptions,
 ) => {
   let importViews: adminViews = {};
-  if (!pluginOptions || !pluginOptions?.excludeCollections)
-    return {
-      Import: {
-        Component: ViewWrapper,
-        path: "/collections/:slug/import",
-      },
+  let paths: string[] = [];
+  if (!pluginOptions || !pluginOptions?.excludeCollections) {
+    paths.push("/collections/:slug/import");
+  } else {
+    paths = config
+      .collections!.filter(
+        (collection) => !pluginOptions.excludeCollections?.includes(collection.slug),
+      )
+      .map((collection) => `/collections/:${collection.slug}/import`);
+  }
+  paths.forEach((path) => {
+    importViews[`Import${path}`] = {
+      Component: ViewWrapper,
+      path,
     };
-  config
-    .collections!.filter(
-      (collection) => !pluginOptions.excludeCollections?.includes(collection.slug),
-    )
-    .forEach((collection) => {
-      importViews[`Import${collection.slug}`] = {
-        Component: ViewWrapper,
-        path: `/collections/:${collection.slug}/import`,
-      };
-    });
-
+  });
   return importViews;
 };
